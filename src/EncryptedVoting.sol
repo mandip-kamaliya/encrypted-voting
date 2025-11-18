176b460dbf325853a31bb4052e3064afb21f5707
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-11-18 16:36:13 +0530

    FIX:ci/cd error

--- src/EncryptedVoting.sol
+++ src/EncryptedVoting.sol
@@ -5,32 +5,30 @@ pragma solidity ^0.8.25;
 import "@fhenixprotocol/cofhe-contracts/FHE.sol";
 
 //type euint8 is uint256;
-contract EncryptedVoting{
+contract EncryptedVoting {
     euint8 public encryptedtally;
 
     address public immutable i_owner;
 
-    bool public decriptRequested=false;
+    bool public decriptRequested = false;
 
     euint8 private ENCRYPTED_ZERO;
 
     event VoteCast(address indexed voter);
-    
+
     constructor() {
         i_owner = msg.sender;
 
         ENCRYPTED_ZERO = FHE.asEuint8(0);
         FHE.allowThis(ENCRYPTED_ZERO);
-        encryptedtally=ENCRYPTED_ZERO;
+        encryptedtally = ENCRYPTED_ZERO;
         FHE.allowThis(encryptedtally);
-
     }
 
-    function castVote(InEuint8 calldata encryptedVote) public{
-       euint8 vote = FHE.asEuint8(encryptedVote);
-       encryptedtally = FHE.add(encryptedtally,vote);
-       FHE.allowThis(encryptedtally);
-       emit VoteCast(msg.sender);
+    function castVote(InEuint8 calldata encryptedVote) public {
+        euint8 vote = FHE.asEuint8(encryptedVote);
+        encryptedtally = FHE.add(encryptedtally, vote);
+        FHE.allowThis(encryptedtally);
+        emit VoteCast(msg.sender);
     }
-
 }
