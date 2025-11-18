a3e95432d6a86b50f963c5c77c58236c73471eb4
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-11-18 16:19:11 +0530

    added state variables

--- src/EncryptedVoting.sol
+++ src/EncryptedVoting.sol
@@ -6,5 +6,15 @@ import "@fhenixprotocol/cofhe-contracts/FHE.sol"
 
 type euint8 is uint256;
 contract EncryptedVoting{
+    euint8 public encryptedtally;
+
+    address public immutable i_owner;
+
+    bool public decriptrequested;
+
+    euint8 public ENCRYPTED_ZERO;
+
+    event VoteCast(address indexed voter);
+
 
 }
